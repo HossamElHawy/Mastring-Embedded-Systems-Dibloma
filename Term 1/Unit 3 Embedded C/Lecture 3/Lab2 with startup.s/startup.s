9a4061869e456acdf3382844eb0522acc844c42b
HossamElHawy <eng.hossamelhawy@gmail.com>
2023-10-28 11:26:23 +0200

    Lecture 3 Embedded Lab How To Toggle Led With STM32F103C6

---
+++ Term 1/Unit 3 Embedded C/Lecture 3/Lab2 with startup.s/startup.s
@@ -0,0 +1,18 @@
+/*Startup_CortexM3
+Eng.Hossam
+*/
+.section .Vectors
+.word 0x20001000
+.word _reset
+.word _Vector_Handler  // _NMI_Handler
+.word _Vector_Handler  //_Hard_Fault_Handler
+//SRAM 0x20000000
+
+.section .text
+_reset:
+     bl main
+     b  .
+.thumb_func
+_Vector_Handler:
+     b _reset
+
